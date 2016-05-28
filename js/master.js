c0eaf92ab1d187bff65ae647259c41a130997178
BuonOmo <buonomo.ulysse@gmail.com>
2016-05-28 16:35:05 +0200

    add non-empty cards !
     - shuffle give 8 cards to each player
     - card done on click
     - cards animation when done
     - css comment sections

--- js/master.js
+++ js/master.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var boardSize = 5;
+var boardCards = [4][4];
+
 /**
  * Create a board by filling it with squares.
  * @param  DOM      board  the element on the dom to fill
@@ -31,11 +34,30 @@ function fillStack (stack,color,number) {
   }
 };
 
+/**
+ * Create 16 cards and append 8 to #player1 and 8 to #player2
+ */
 function shuffleCards () {
   for (var i = 0; i < 16; i++) {
     var player = Math.round(Math.random())+1;
-    console.log(player);
-    $('<div class="card"></div>').attr('value',i).appendTo('#player'+player);
+    var color = [];
+    var c = 0;
+    for (var j = 1; j < 16; j*=2) {
+      color[c++] = (i & j) ? 'black' : 'white';
+    }
+    $('<div class="card"></div>')
+      .attr('value',i)
+      .append('<div class="'+color[0]+' small-pawn" style="bottom:0%;left:50%"></div>')
+      .append('<div class="'+color[1]+' small-pawn" style="bottom:50%;left:50%"></div>')
+      .append('<div class="'+color[2]+' small-pawn" style="bottom:0%;left:0%"></div>')
+      .append('<div class="'+color[3]+' small-pawn" style="bottom:50%;left:0%"></div>')
+      .appendTo('#player'+player);
+  }
+  while ($('#player1 .card').length > 8) {
+    $($('#player1 .card')[Math.round(Math.random()*$('#player1 .card').length)]).appendTo('#player2');
+  }
+  while ($('#player2 .card').length > 8) {
+    $($('#player2 .card')[Math.round(Math.random()*$('#player2 .card').length)]).appendTo('#player1');
   }
 }
 
@@ -43,7 +65,28 @@ shuffleCards();
 
 fillStack($('.stack:first'),'black',13);
 fillStack($('.stack:last'),'white',13);
-createBoard($('.board:first'),5);
+createBoard($('.board:first'),boardSize);
+
+$('.card').click(function () {
+    $(this).addClass('done');
+});
+
+$('.card').on('animationstart', function (event) {
+  var elmt = this;
+  setTimeout(function () {
+    var left, bottom;
+    for (var i = 0; i < 4; i++) {
+      bottom = !(i % 2) ? '50%' : '75%';
+      left   =  (i > 1) ? '50%' : '75%';
+      $($(elmt).children()[i])
+        .css('bottom',bottom)
+        .css('left',left)
+        .css('margin','2%')
+        .css('width','21%')
+        .css('height','21%');
+    }
+  },1000);
+})
 
 $('pawn').draggable({
   containment: '.board .stack',
