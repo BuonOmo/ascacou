5db09539394a29fd8398a282f97bfc1676a651c4
BuonOmo <buonomo.ulysse@gmail.com>
2016-05-28 18:21:26 +0200

    update state on board
    - add var cards and the encoding explanation
    - add function updateSate that find all cards done

--- js/master.js
+++ js/master.js
@@ -1,7 +1,65 @@
 'use strict';
 
 var boardSize = 5;
-var boardCards = [4][4];
+/**
+ * All possible cards played on the boad, encoded on 8 bit this way :
+ *
+ * for a card with slots in this order ->
+ * | A | B |
+ * | C | D |
+ *
+ * and bits like — a b c d e f g h — (a is most significant bit)
+ *
+ *   - a, b, c and d are used to describe if slots are full or empty :
+ * | X |   |
+ * |   |   | is 0001
+ *
+ * | X | X |
+ * |   |   | is 0011
+ *
+ * | X | X |
+ * | X | X | is 1111
+ *
+ * slot 1 is encoder with d, 1 for full, 0 for empty.
+ * slot 2 with c
+ * slot 3 with b
+ * slot 4 with a
+ *
+ * - e, f, g and h are used to describe color :
+ * black is 1 and white is 0, the method is the same as abcd encoding
+ * @type {Array}
+ */
+var cards = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+const STACK_ENCODE = 0b11110000;
+const CARD_ENCODE  = 0b00001111;
+
+const A_FULL = 0b00010000;
+const B_FULL = 0b00100000;
+const C_FULL = 0b01000000;
+const D_FULL = 0b10000000;
+
+const A_BLACK = 0b00000001;
+const B_BLACK = 0b00000010;
+const C_BLACK = 0b00000100;
+const D_BLACK = 0b00001000;
+
+/**
+ * @return boolean state of card at index <index> on the board.
+ */
+function isFull(index) {
+  return (cards[index] & STACK_ENCODE) == STACK_ENCODE;
+};
+
+/**
+ * @return boolean state of card at index <index> on the board.
+ */
+function isEmpty(index) {
+  return (cards[index] & STACK_ENCODE) == 0;
+};
+
+function getCard(index) {
+  return (cards[index] & CARD_ENCODE);
+}
 
 /**
  * Create a board by filling it with squares.
@@ -15,8 +73,11 @@ function createBoard (board,size) {
   for (var i = 0; i < size; i++) {
     for (var j = 0; j < size; j++) {
       var left = Math.round(100 * j/size);
-      var bottom = Math.round(100 * i/size);
-      $('<square></square>').css('left',left+'%').css('bottom',bottom+'%').appendTo('.board');
+      var bottom = Math.round(100 * (size-i-1)/size);
+      $('<square index="'+(size*i + j)+'"></square>')
+        .css('left',left+'%')
+        .css('bottom',bottom+'%')
+        .appendTo('.board');
     }
   }
 };
@@ -47,10 +108,10 @@ function shuffleCards () {
     }
     $('<div class="card"></div>')
       .attr('value',i)
-      .append('<div class="'+color[0]+' small-pawn" style="bottom:0%;left:50%"></div>')
+      .append('<div class="'+color[0]+' small-pawn" style="bottom:50%;left:0%"></div>')
       .append('<div class="'+color[1]+' small-pawn" style="bottom:50%;left:50%"></div>')
       .append('<div class="'+color[2]+' small-pawn" style="bottom:0%;left:0%"></div>')
-      .append('<div class="'+color[3]+' small-pawn" style="bottom:50%;left:0%"></div>')
+      .append('<div class="'+color[3]+' small-pawn" style="bottom:0%;left:50%"></div>')
       .appendTo('#player'+player);
   }
   while ($('#player1 .card').length > 8) {
@@ -62,10 +123,19 @@ function shuffleCards () {
 }
 
 shuffleCards();
-
 fillStack($('.stack:first'),'black',13);
 fillStack($('.stack:last'),'white',13);
 createBoard($('.board:first'),boardSize);
+
+/**
+ * updateCards update all the "cards" vector around the modified index
+ * @param  number index   square that has been modified
+ * @param  number value   1 for black, 0 for white
+ * @return {[type]}       [description]
+ */
+function updateCards (index,value) {
+
+}
 
 $('.card').click(function () {
     $(this).addClass('done');
@@ -114,5 +184,57 @@ function handlerDrop (event, ui) {
     ui.draggable.css('position','absolute');
     ui.draggable.draggable('disable');
     $(this).append(ui.draggable);
+    // var color = ui.draggable.hasClass('white') ? 0 : 1;
+    updateState();
   }
 }
+
+function updateState () {
+  for (var i = 0; i < cards.length; i++) {
+    if ($('square[index='+cardsToBoardIndex(i)+']').is(':parent')) {
+      cards[i] = cards[i] | A_FULL;
+      if ($('square[index='+(cardsToBoardIndex(i))+']>pawn').hasClass('black'))
+				cards[i] = cards[i] | A_BLACK;
+    }
+
+    if ($('square[index='+(cardsToBoardIndex(i)+1)+']').is(':parent')) {
+      cards[i] = cards[i] | B_FULL;
+      if ($('square[index='+(cardsToBoardIndex(i)+1)+']>pawn').hasClass('black'))
+				cards[i] = cards[i] | B_BLACK;
+    }
+
+    if ($('square[index='+(cardsToBoardIndex(i)+boardSize)+']').is(':parent')) {
+      cards[i] = cards[i] | C_FULL;
+      if ($('square[index='+(cardsToBoardIndex(i)+boardSize)+']>pawn').hasClass('black'))
+				cards[i] = cards[i] | C_BLACK;
+    }
+
+    if ($('square[index='+(cardsToBoardIndex(i)+boardSize+1)+']').is(':parent')) {
+      cards[i] = cards[i] | D_FULL;
+      if ($('square[index='+(cardsToBoardIndex(i)+boardSize+1)+']>pawn').hasClass('black'))
+        cards[i] = cards[i] | D_BLACK;
+    }
+
+    if (isFull(i)) {
+      $('.card[value='+getCard(i)+']').addClass('done');
+    }
+  }
+}
+
+
+function cardsToBoardIndex(i) {
+  return i + Math.floor(i/4);
+}
+/**
+ * TODO: implement this if needed
+ * return all indexes to check in the "cards" array
+ * @param  number board_index  index that has been modified
+ * @return array               indexes to check it cards
+ */
+/*
+function toCheck(board_index) {
+  var ret = [];
+  if (board_index < 20)
+  ret.push(index - Math.round(index/5));
+}
+*/
